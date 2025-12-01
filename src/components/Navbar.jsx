f569be25b6bc3aac5422df7db4f1c20b4cbdcf97
Olti Bekteshi <oltib@login.al>
2025-12-01 16:51:11 +0100

    Fixed bell icon on notifications

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,6 @@ const Navbar = () => {
     load();
   }, [user]);
 
-  
   useEffect(() => {
     if (!user) return;
 
@@ -68,23 +67,39 @@ const Navbar = () => {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
-
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-linear-to-r from-blue-500 to-green-900 shadow-md">
-      
       <div className="max-w-7xl mx-auto flex items-center justify-between p-4 text-white">
-        <Link to="/" className="text-xl font-bold hover:opacity-80 flex items-center">
+        <Link
+          to="/"
+          className="text-xl font-bold hover:opacity-80 flex items-center"
+        >
           <img src="/movie.png" alt="Logo" className="h-7 w-7 mr-1" />
           MovieTracker
         </Link>
 
-
         <div className="hidden md:flex items-center gap-5">
-
-          <Link to="/" className="px-3 py-2 rounded-2xl hover:opacity-[0.7]">Ballina</Link>
-          <Link to="/movies" className="px-3 py-2 rounded-2xl hover:opacity-[0.7]">Filmat</Link>
-          <Link to="/watchlist" className="px-3 py-2 rounded-2xl hover:opacity-[0.7]">Të shikuar</Link>
-          <Link to="/watch-later" className="px-3 py-2 rounded-2xl hover:opacity-[0.7]">Shiko më vonë</Link>
+          <Link to="/" className="px-3 py-2 rounded-2xl hover:opacity-[0.7]">
+            Ballina
+          </Link>
+          <Link
+            to="/movies"
+            className="px-3 py-2 rounded-2xl hover:opacity-[0.7]"
+          >
+            Filmat
+          </Link>
+          <Link
+            to="/watchlist"
+            className="px-3 py-2 rounded-2xl hover:opacity-[0.7]"
+          >
+            Të shikuar
+          </Link>
+          <Link
+            to="/watch-later"
+            className="px-3 py-2 rounded-2xl hover:opacity-[0.7]"
+          >
+            Shiko më vonë
+          </Link>
 
           {user && (
             <div className="relative" ref={notifRef}>
@@ -95,9 +110,7 @@ const Navbar = () => {
               />
 
               {notifications.length > 0 && (
-                <span className="absolute -top-1 -right-1 bg-red-600 text-white text-xs px-2 py-0.5 rounded-full">
-                  {notifications.length}
-                </span>
+                <span className="absolute top-0 -right-1 bg-red-600 h-2 w-2 rounded-full"></span>
               )}
 
               {showNotif && (
@@ -105,7 +118,9 @@ const Navbar = () => {
                   <h3 className="font-bold mb-2">Rekomandimet</h3>
 
                   {notifications.length === 0 ? (
-                    <p className="text-sm text-gray-500">Nuk keni rekomandime.</p>
+                    <p className="text-sm text-gray-500">
+                      Nuk keni rekomandime.
+                    </p>
                   ) : (
                     notifications.map((n) => (
                       <div
@@ -123,17 +138,23 @@ const Navbar = () => {
           )}
 
           <SignedOut>
-            <Link to="/sign-in" className="border px-4 py-2 rounded-2xl">Kyqu</Link>
-            <Link to="/sign-up" className="border px-4 py-2 rounded-2xl">Krijo llogari</Link>
+            <Link to="/sign-in" className="border px-4 py-2 rounded-2xl">
+              Kyqu
+            </Link>
+            <Link to="/sign-up" className="border px-4 py-2 rounded-2xl">
+              Krijo llogari
+            </Link>
           </SignedOut>
 
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
           </SignedIn>
-
         </div>
 
-        <button onClick={() => setIsOpen(!isOpen)} className="md:hidden text-2xl">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="md:hidden text-2xl"
+        >
           {isOpen ? <FiX /> : <FiMenu />}
         </button>
       </div>
@@ -141,63 +162,84 @@ const Navbar = () => {
       {isOpen && (
         <div className="md:hidden bg-linear-to-r from-blue-500 to-green-900 px-4 pb-4 text-white space-y-3">
           {user && (
-  <div className="relative" ref={notifRef}>
-    <FiBell
-      size={26}
-      className="cursor-pointer"
-      onClick={() => setShowNotif((prev) => !prev)}
-    />
+            <div className="relative" ref={notifRef}>
+              <FiBell
+                size={26}
+                className="cursor-pointer"
+                onClick={() => setShowNotif((prev) => !prev)}
+              />
 
-    {notifications.length > 0 && (
-      <span className="absolute -top-1 -right-1 bg-red-600 text-white text-xs px-2 py-0.5 rounded-full">
-        {notifications.length}
-      </span>
-    )}
+              {notifications.length > 0 && (
+                <span className="absolute -top-1 -right-1 bg-red-600 text-white text-xs px-2 py-0.5 rounded-full">
+                  {notifications.length}
+                </span>
+              )}
 
-    {showNotif && (
-      <div className="absolute left-0 mt-2 bg-white text-black w-72 rounded-xl shadow-xl p-3 z-50">
-        <h3 className="font-bold mb-2">Rekomandimet</h3>
+              {showNotif && (
+                <div className="absolute left-0 mt-2 bg-white text-black w-72 rounded-xl shadow-xl p-3 z-50">
+                  <h3 className="font-bold mb-2">Rekomandimet</h3>
 
-        {notifications.length === 0 ? (
-          <p className="text-sm text-gray-500">Nuk keni rekomandime.</p>
-        ) : (
-          notifications.map((n) => (
-            <div
-              key={n.id}
-              onClick={() => openRecommended(n.movie_id)}
-              className="bg-green-600 p-2 rounded-lg mb-2 text-sm font-bold text-white cursor-pointer hover:bg-green-700"
-            >
-              {n.message}
+                  {notifications.length === 0 ? (
+                    <p className="text-sm text-gray-500">
+                      Nuk keni rekomandime.
+                    </p>
+                  ) : (
+                    notifications.map((n) => (
+                      <div
+                        key={n.id}
+                        onClick={() => openRecommended(n.movie_id)}
+                        className="bg-green-600 p-2 rounded-lg mb-2 text-sm font-bold text-white cursor-pointer hover:bg-green-700"
+                      >
+                        {n.message}
+                      </div>
+                    ))
+                  )}
+                </div>
+              )}
             </div>
-          ))
-        )}
-      </div>
-    )}
-  </div>
-)}
-
+          )}
 
           <Link to="/" className="block py-2" onClick={() => setIsOpen(false)}>
             Ballina
           </Link>
 
-          <Link to="/movies" className="block py-2" onClick={() => setIsOpen(false)}>
+          <Link
+            to="/movies"
+            className="block py-2"
+            onClick={() => setIsOpen(false)}
+          >
             Filmat
           </Link>
 
-          <Link to="/watchlist" className="block py-2" onClick={() => setIsOpen(false)}>
+          <Link
+            to="/watchlist"
+            className="block py-2"
+            onClick={() => setIsOpen(false)}
+          >
             Të shikuar
           </Link>
 
-          <Link to="/watch-later" className="block py-2" onClick={() => setIsOpen(false)}>
+          <Link
+            to="/watch-later"
+            className="block py-2"
+            onClick={() => setIsOpen(false)}
+          >
             Shiko më vonë
           </Link>
 
           <SignedOut>
-            <Link to="/sign-in" className="block py-2" onClick={() => setIsOpen(false)}>
+            <Link
+              to="/sign-in"
+              className="block py-2"
+              onClick={() => setIsOpen(false)}
+            >
               Kyqu
             </Link>
-            <Link to="/sign-up" className="block py-2" onClick={() => setIsOpen(false)}>
+            <Link
+              to="/sign-up"
+              className="block py-2"
+              onClick={() => setIsOpen(false)}
+            >
               Krijo llogari
             </Link>
           </SignedOut>
