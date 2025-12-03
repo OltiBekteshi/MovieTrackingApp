63d01cabbeb477886d0e80db83c461a44d6fd91e
Olti Bekteshi <oltib@login.al>
2025-12-03 16:45:02 +0100

    Removed footer elements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,32 +3,11 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-linear-to-r from-blue-500  to-green-900 shadow-md text-[#C5C6C7] py-6  border-t  border-white">
-      <div className="max-w-6xl mx-auto px-4 flex flex-col sm:flex-row justify-between items-center gap-3 text-sm">
+      <div className="max-w-6xl mx-auto px-4 flex flex-col sm:flex-row justify-center items-center text-sm">
         <p className="text-center sm:text-left text-white font-bold">
           © {new Date().getFullYear()} MovieTracker. Të gjitha të drejtat të
           rezervuara.
         </p>
-
-        <div className="flex gap-4">
-          <a
-            href="#"
-            className=" transition-colors duration-200 text-white font-bold hover:opacity-[0.7]"
-          >
-            Politika e privatësisë
-          </a>
-          <a
-            href="#"
-            className=" transition-colors duration-200 text-white font-bold hover:opacity-[0.7]"
-          >
-            Kushtet
-          </a>
-          <a
-            href="#"
-            className=" transition-colors duration-200 text-white font-bold hover:opacity-[0.7]"
-          >
-            Kontakti
-          </a>
-        </div>
       </div>
     </footer>
   );
